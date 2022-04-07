a97e1b7f193da831c1a3ecae5a3c80446271ee20
Mattb110 <98766571+Mattb110@users.noreply.github.com>
2022-04-07 16:54:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// initialize swiper js
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+});
